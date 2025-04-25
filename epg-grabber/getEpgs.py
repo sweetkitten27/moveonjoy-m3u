66eea5ab1e11cea8c7c6ecc6b6fa107405cbcdce
sweetkitten27 <lukestot0103@gmail.com>
2025-04-25 14:12:13 -0700

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -64,6 +64,7 @@ urls = [
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_PLEX1.xml.gz',
+    'https://raw.githubusercontent.com/acidjesuz/EPGTalk/master/US_guide.xml.gz',
 ]
 
 if __name__ == "__main__":
